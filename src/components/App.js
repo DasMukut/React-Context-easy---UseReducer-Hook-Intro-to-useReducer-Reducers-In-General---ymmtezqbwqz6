b786975001ed4eed4840460be8c16e536ce2b8bb
DasMukut <108810054+DasMukut@users.noreply.github.com>
2022-10-13 23:38:23 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,15 +1,18 @@
 import React from 'react'
 import '../styles/App.css';
 import { UserProfile } from './UserProfile';
-
+const UserContext = createContext({})
 
 const App = () => {
+  let data={
+    name:"Newton",
+    age:3}
 
   return (
     <div id="main">
-      
+      <UserContext.Provider value={data}>
         <UserProfile />
-      
+       </UserContext.Provider >
     </div>
   )
 }
