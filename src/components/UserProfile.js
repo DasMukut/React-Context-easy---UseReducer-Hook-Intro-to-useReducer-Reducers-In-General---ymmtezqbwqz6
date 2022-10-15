5efd2a1bf745c1160d7aeb69ec24c4e5272de0df
DasMukut <108810054+DasMukut@users.noreply.github.com>
2022-10-15 11:13:36 +0530

    Update UserProfile.js

--- src/components/UserProfile.js
+++ src/components/UserProfile.js
@@ -6,8 +6,8 @@ const UserProfile = () =>{
 const { name, age} = useContext(UserContext);
   return (
     <>
-      <h1 id="name"> Name:-{name} </h1>
-      <h1 id="age"> Age:-{age} </h1>
+      <h1 id="name">`Name:- ${name}`</h1>
+      <h1 id="age">`Age:- ${age}`</h1>
     </>
     )
 }
